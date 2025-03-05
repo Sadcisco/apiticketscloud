c5d12e66c499bdc6fd3bc693ded52fea07ba503a
F5 <panchi09sotov@gmail.com>
2025-03-05 12:52:01 -0300

    a

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     JWT_SECRET_KEY = 'Inicio01*'  # Cambia esto por una clave segura
     SQLALCHEMY_POOL_RECYCLE = 280  # Evita la desconexión por inactividad
     SQLALCHEMY_POOL_TIMEOUT = 30   # Tiempo máximo de espera para conexión
-    SQLALCHEMY_POOL_SIZE = 10      # Número máximo de conexiones activas
+    SQLALCHEMY_POOL_SIZE = 10      # Número maáximo de conexiones activas
     SQLALCHEMY_MAX_OVERFLOW = 20   # Conexiones extra si se alcanza el máximo
     SQLALCHEMY_ENGINE_OPTIONS = {
         'pool_pre_ping': True  # Verifica la conexión antes de cada consulta
